ef51db209e1a5e88810452d004cf4bfb18c3cddc
nhanniee.s <nhanniee.s@gmail.com>
2021-04-02 13:51:05 +0700

    initial

---
+++ vite/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+    <input type="text" placeholder="name" v-model="name" />
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+      name: "",
+    };
+  },
+};
+</script>
