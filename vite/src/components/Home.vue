d993df54abbae3d6d78cfed07bd89f4b524f6048
nhanniee.s <nhanniee.s@gmail.com>
2021-04-03 12:38:00 +0700

    update card

--- vite/src/components/Home.vue
+++ vite/src/components/Home.vue
@@ -41,24 +41,32 @@
     </div>
     <br />
     <!-- card (show images & details) -->
-    <div class="ml">
+    <div class="pt-5">
       <h4>This week:</h4>
-      <div class="ui card">
-        <div class="image">
-          <img src="https://semantic-ui.com/images/avatar2/large/elyse.png" />
-        </div>
-        <div class="content">
-          <a class="header">Kristy</a>
-          <div class="meta">
-            <span class="date">Joined in 2013</span>
+      <div class="card-deck">
+        <div class="ui cards">
+          <div
+            class="ui card"
+            style="min-width: 15rem; max-width: 15rem"
+            v-for="(picture, index) in Gallery"
+            :key="index"
+          >
+            <div class="image">
+              <img :src="'' + picture.pictureURL" />
+            </div>
+            <div class="content">
+              <a class="header">{{ picture.pictureName }}</a>
+              <div class="meta">
+                {{ picture.username }}
+              </div>
+              <div class="description">{{ picture.pictureDetail }}</div>
+            </div>
+            <div class="card-footer">
+              <p class="card-text">
+                <small class="text-muted">Close : {{ picture.closedate }}</small>
+              </p>
+            </div>
           </div>
-          <div class="description">Kristy is an art director living in New York.</div>
-        </div>
-        <div class="extra content">
-          <a>
-            <i class="user icon"></i>
-            22 Friends
-          </a>
         </div>
       </div>
     </div>
@@ -68,6 +76,84 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      Gallery: [
+        {
+          firstname: "Elyse",
+          lastname: "Emma",
+          username: "@handsomeelyse",
+          pictureName: "",
+          pictureURL: "https://semantic-ui.com/images/avatar2/large/elyse.png",
+          pictureDetail: "I cut my hair 'cus you don't care my heart",
+          startprice: 100,
+          currentprice: 100,
+          closedate: "01/01/2028, 16:00PM",
+          status: "available",
+        },
+        {
+          firstname: "Molly",
+          lastname: "May",
+          pictureName: "",
+          username: "@longhairbutdontcare",
+          pictureURL: "https://semantic-ui.com/images/avatar2/large/molly.png",
+          pictureDetail: "please ignore my username",
+          startprice: 100,
+          currentprice: 100,
+          closedate: "01/01/2028, 16:00PM",
+          status: "available",
+        },
+        {
+          firstname: "Matthew",
+          lastname: "Brew",
+          username: "@madmatthew",
+          pictureName: "",
+          pictureURL: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+          pictureDetail: "I'm not mad'",
+          startprice: 100,
+          currentprice: 100,
+          closedate: "01/01/2028, 16:00PM",
+          status: "available",
+        },
+        {
+          firstname: "Kristy",
+          lastname: "Cream",
+          username: "@ilovedonut",
+          pictureName: "",
+          pictureURL: "https://semantic-ui.com/images/avatar2/large/kristy.png",
+          pictureDetail: "not Krispy Kreme but Kristy Cream",
+          startprice: 100,
+          currentprice: 100,
+          closedate: "01/01/2028, 16:00PM",
+          status: "available",
+        },
+        {
+          firstname: "XXX",
+          lastname: "XXX",
+          username: "@handsomeelyse",
+          pictureName: "",
+          pictureURL: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+          pictureDetail: "I cut my hair 'cus you don't care my heart",
+          startprice: 100,
+          currentprice: 100,
+          closedate: "01/01/2028, 16:00PM",
+          status: "available",
+        },
+        {
+          firstname: "XXX",
+          lastname: "XXX",
+          username: "@handsomeelyse",
+          pictureName: "",
+          pictureURL: "https://semantic-ui.com/images/avatar2/large/kristy.png",
+          pictureDetail: "I cut my hair 'cus you don't care my heart",
+          startprice: 100,
+          currentprice: 100,
+          closedate: "01/01/2028, 16:00PM",
+          status: "available",
+        },
+      ],
+    };
+  },
 };
 </script>
 
