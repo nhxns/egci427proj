ce2b9b9b463df8e4057a548ba92d754b22213c6d
nhanniee.s <nhanniee.s@gmail.com>
2021-04-05 13:05:02 +0700

    update responsive

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -7,15 +7,15 @@
         <button
           class="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarText">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="/">Home</a>
@@ -27,7 +27,7 @@
               <a class="nav-link" href="">Sell</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="">History</a>
+              <a class="nav-link" href="">Profile</a>
             </li>
           </ul>
         </div>
